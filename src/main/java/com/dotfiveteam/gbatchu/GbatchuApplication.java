94fa70f4948dc0c5a7b0c69b1fad512cab05e820
RobinRheem <roborheems@gmail.com>
2019-06-20 20:10:35 +0900

    Init Spring Batch application

---
+++ src/main/java/com/dotfiveteam/gbatchu/GbatchuApplication.java
@@ -0,0 +1,13 @@
+package com.dotfiveteam.gbatchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbatchuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbatchuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotfiveteam/gbatchu/GbatchuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dotfiveteam.gbatchu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GbatchuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
